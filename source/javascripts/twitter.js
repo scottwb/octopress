4960d2a4026e8ae38cfeab9fca0c0f112bde6634
Scott W. Bradley <scottwb@scottwb.net>
2012-01-21 17:46:05 -0800

    Fix twitter plugin. It was crashing on poor array enumaration technique.

--- source/javascripts/twitter.js
+++ source/javascripts/twitter.js
@@ -60,8 +60,7 @@ function linkifyTweet(text, url) {
 function showTwitterFeed(tweets, twitter_user) {
   var timeline = document.getElementById('tweets'),
       content = '';
-
-  for (var t in tweets) {
+  for (var t=0, len=tweets.length; t<len; ++t) {
     content += '<li>'+'<p>'+'<a href="http://twitter.com/'+twitter_user+'/status/'+tweets[t].id_str+'">'+prettyDate(tweets[t].created_at)+'</a>'+linkifyTweet(tweets[t].text.replace(/\n/g, '<br>'), tweets[t].entities.urls)+'</p>'+'</li>';
   }
   timeline.innerHTML = content;
